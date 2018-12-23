ffae6183926627ba54361a4851a143647caa368e
Tim <tim@Tims-MacBook-Pro.local>
2018-12-22 18:26:38 -0800

    APITest.py

---
+++ Page.py
@@ -0,0 +1,8 @@
+
+
+class PageInfo(object):
+
+    def __init__(self):
+        self.baseURL = "https://api.nasa.gov/planetary/apod"
+        self.apiKey = "bXpso712pAN1Ir8jpVZ0Nz4Mvt9HOG1S9mNeFAgF"
+
